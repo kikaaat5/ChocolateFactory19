8564e83f61a351b80951588997fd3b3c64da0899
Anastasija Djuric <anastasijadjuric31@gmail.com>
2024-06-06 19:39:33 +0200

    Resolved conflicts

--- WebShop/Frontend/src/router/index.js
+++ WebShop/Frontend/src/router/index.js
@@ -2,7 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Login from '@/views/Login.vue'
 import WebShop from '@/views/WebShop.vue'
-import FactoryDetails from '@/views/FactoryDetails.vue'
+import ChocolateUpdateView from '@/views/ChocolateView.vue'
+import ChocolateAddView from '@/views/ChocolateAddView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,18 +25,22 @@ const router = createRouter({
       component: WebShop
     },
     {
+      path: '/chocolate:chocolateId',
+      name: 'ChocolateUpdate',
+      component: ChocolateUpdateView
+    },
+    {
+      path: '/chocolateAdd',
+      name: 'ChocolateAdd',
+      component: ChocolateAddView
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    },
-    {
-      path:'/factory/:id',
-      name: 'factoryDetails',
-      component: FactoryDetails,
-      props:true
     }
   ]
 })
---
+++ WebShop/Frontend/src/views/ChocolateAddView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import ChocolateAdd from '../components/ChocolateAdd.vue'
+</script>
+
+<template>
+  <main>
+    <ChocolateAdd />
+  </main>
+</template>
